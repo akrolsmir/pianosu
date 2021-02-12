2e1bea71917186bc2e0366f8f129fbb37e0dc4d5
Austin Chen <akrolsmir@gmail.com>
2021-02-12 13:44:06 -0600

    Copy Phaser3 Multitouch Piano

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <HelloWorld />
+  <Piano />
 </template>
 
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
+import Piano from './components/Piano.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
