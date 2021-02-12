9f6df9b397f01e6dc00e520bc921a18fb02f88a9
Austin Chen <akrolsmir@gmail.com>
2021-02-12 13:17:02 -0600

    Add the phaser Hello World

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,75 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h2>Pianosu!</h2>
+  <div id="gameDiv" style="margin: 0 auto"></div>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
+<script>
+import Phaser from "phaser";
 
-defineProps({
-  msg: String
-})
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+      // See: http://phaser.io/examples/v3/view/physics/arcade/custom-debug-colors
+      debug: true,
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+  parent: "gameDiv",
+};
 
-const state = reactive({ count: 0 })
-</script>
+function preload() {
+  this.load.setBaseURL("https://labs.phaser.io");
 
-<style scoped>
-a {
-  color: #42b983;
+  this.load.image("sky", "assets/skies/space3.png");
+  this.load.image("logo", "assets/sprites/phaser3-logo.png");
+  this.load.image("red", "assets/particles/red.png");
 }
-</style>
+
+let logo;
+function create() {
+  this.add.image(400, 300, "sky");
+
+  var particles = this.add.particles("red");
+
+  var emitter = particles.createEmitter({
+    speed: 1,
+    scale: { start: 1, end: 0 },
+    blendMode: "ADD",
+  });
+
+  logo = this.physics.add.image(400, 100, "logo");
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
+
+function update() {
+  const cursors = this.input.keyboard.createCursorKeys();
+
+  if (cursors.left.isDown) {
+    logo.setVelocityX(s0);
+  }
+
+  if (cursors.right.isDown) {
+    logo.setVelocityX(200);
+  }
+}
+
+export default {
+  mounted() {
+    var game = new Phaser.Game(config);
+  },
+};
+</script>
