1b8b0d1d6690531655b70bc1c01325bf8affd096
Austin Chen <akrolsmir@gmail.com>
2021-02-13 10:53:49 -0600

    Play back extracted melody with ToneJS oscillator

---
+++ src/components/Playback.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Playback extracted melody</h2>
+    <button @click="play">Play music!</button>
+  </div>
+</template>
+
+<script>
+import * as Tone from 'tone'
+import summertime15v from '../summertime15-voice.js'
+
+export default {
+  methods: {
+    play() {
+      const osc = new Tone.Oscillator().toDestination()
+
+      osc.frequency.value = 400
+      for (let i = 0; i < summertime15v.length; i++) {
+        const time = i / 400 // Each tick is 1/400 of a second
+        osc.frequency.linearRampToValueAtTime(summertime15v[i], time)
+      }
+      osc.start().stop('+16')
+    },
+  },
+}
+</script>
