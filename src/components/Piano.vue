86b2fdda0d8df666010833c1a381709ed459199c
Austin Chen <akrolsmir@gmail.com>
2021-02-14 10:16:21 -0600

    Calculate hitobj locations manually

--- src/components/Piano.vue
+++ src/components/Piano.vue
@@ -10,11 +10,25 @@
 
 <script>
 // Demo copied from https://github.com/photonstorm/phaser3-examples/blob/master/public/src/input/multitouch/multi%20touch%20test.js
-import Phaser from 'phaser'
+import Phaser, { Scene } from 'phaser'
 import * as Tone from 'tone'
 import { summertimeVoice, transposeCtoD } from '../s15v-notes'
 
 const SUMMERTIME_BPM = 123
+
+const PIANO_TO_KEYBOARD = {
+  A3: 'A',
+  B3: 'S',
+  C4: 'D',
+  D4: 'F',
+  E4: 'J',
+  F4: 'K',
+  G4: 'L',
+  A4: ';',
+  B4: "'",
+}
+const KEYBOARD_TO_PIANO = invert(PIANO_TO_KEYBOARD)
+const NOTES = Object.keys(PIANO_TO_KEYBOARD)
 
 function invert(obj) {
   return Object.fromEntries(Object.entries(obj).map(([k, v]) => [v, k]))
@@ -32,6 +46,7 @@ var config = {
     extend: {
       createPiano: createPiano,
     },
+    update: update,
   },
   physics: {
     default: 'arcade',
@@ -40,7 +55,49 @@ var config = {
       // debug: true,
     },
   },
+  // TODO: input: {keyboard: {target: window}} ?
   parent: 'gameDiv',
+}
+
+const FALL_VELOCITY = 600 / 1000 // px/ms
+
+/**
+ * hitObj: {
+ *   note: 'D' // or 1, or 'F#4'?
+ *   hitTime: 12345 // ms, when it should be hit
+ *   rect: Phaser.GameObjects.Rectangle ref
+ *   render(time) => { adjust rect's position }
+ *   hit(time)?? => { replace with score object }
+ * }
+ */
+function makeHitObject(note, hitTime, /** @type {Phaser.Scene} */ scene) {
+  const rect = scene.add.rectangle(-100, -100, 30, 30, 0xaa4422)
+  rect.setDepth(100)
+  return {
+    note,
+    hitTime,
+    rect,
+    render(time) {
+      const TARGET_Y = 525 + 25
+      const x = NOTES.indexOf(note) * 85 + 35 + 25
+      // At hitTime, y should be 800; at hitTime - 1s; y should be 800 - FALL_VELOCITY
+      const y = TARGET_Y - (hitTime - time) * FALL_VELOCITY
+      rect.setPosition(x, y)
+    },
+  }
+}
+
+let HIT_OBJS
+
+// Time is msecs, delta is time since last update
+function update(time, delta) {
+  /** @type {Phaser.Scene} */
+  const scene = this
+
+  const lastStart = scene.data.get('lastStart')
+  const playTime = time - lastStart
+
+  HIT_OBJS.forEach((hitObj) => hitObj.render(playTime))
 }
 
 function preload() {
@@ -67,6 +124,17 @@ function create() {
   } else {
     this.createPiano()
   }
+
+  // Initialize the hit objects for this song
+  const tune = summertimeVoice
+  HIT_OBJS = []
+  for (const [i, note] of tune.entries()) {
+    const SUMMERTIME_OFFSET = 1200 // If visuals hit before audio, make this bigger
+    const eigthNoteMs = (60 * 1000) / SUMMERTIME_BPM / 2
+    const time = SUMMERTIME_OFFSET + eigthNoteMs * i
+    // TODO: maybe handle simultaneous notes
+    HIT_OBJS.push(makeHitObject(note, time, this))
+  }
 }
 
 /** @type {Tone.PolySynth} */
@@ -86,19 +154,6 @@ function createPiano() {
   }
 
   // Play white notes on keypress
-  const pianoToKeyboard = {
-    A3: 'A',
-    B3: 'S',
-    C4: 'D',
-    D4: 'F',
-    E4: 'J',
-    F4: 'K',
-    G4: 'L',
-    A4: ';',
-    B4: "'",
-  }
-  const keyboardToPiano = invert(pianoToKeyboard)
-  const notes = Object.keys(pianoToKeyboard)
 
   // Map keyboard buttons to the right keycode
   // See https://photonstorm.github.io/phaser3-docs/Phaser.Input.Keyboard.KeyCodes.html
@@ -109,14 +164,14 @@ function createPiano() {
   function reverseKeyCode(code) {
     return invert(keyMap)[code] || code
   }
-  const keyboardString = Object.keys(keyboardToPiano).map(keyCode).join(',')
+  const keyboardString = Object.keys(KEYBOARD_TO_PIANO).map(keyCode).join(',')
   const keyObjects = scene.input.keyboard.addKeys(keyboardString)
 
   synth = new Tone.PolySynth(Tone.Synth).toDestination()
   for (const [keyCode, keyObject] of Object.entries(keyObjects)) {
     keyObject.on('down', () => {
       const keyboard = reverseKeyCode(keyCode)
-      const note = keyboardToPiano[keyboard]
+      const note = KEYBOARD_TO_PIANO[keyboard]
       const transposed = transposeCtoD(note)
       synth.triggerAttackRelease(transposed, '8n')
 
@@ -131,22 +186,15 @@ function createPiano() {
   moreKeys.on('down', () => {
     if (!music.isPlaying) {
       music.play()
-      scene.time.paused = false
-
-      // Start showing the beatmap
-      scene.time.addEvent({
-        delay: 0, //ms
-        callback: startBeatmap,
-      })
+      scene.data.set('lastStart', scene.time.now)
     } else {
-      music.resume()
-      scene.time.paused = true
-      scene.time.timeScale
+      music.pause()
+      scene.data.set('lastStart', Infinity)
     }
   })
 
   function addHitBlock(note) {
-    const x = notes.indexOf(note) * 85 + 35
+    const x = NOTES.indexOf(note) * 85 + 35
     const y = 525
     const rect = new Phaser.Geom.Rectangle(x, y, 50, 50)
     const graphics = scene.add.graphics()
@@ -155,46 +203,12 @@ function createPiano() {
     setTimeout(() => graphics.destroy(), 100)
   }
 
-  // Show falling blocks over a looping pattern
-  const tune = summertimeVoice
-
-  let index = 0
-  function nextNote() {
-    let note = tune[index]
-    index = (index + 1) % tune.length
-    if (Array.isArray(note)) {
-      // Group of notes to play
-      note.map(addFallingBlock)
-    } else if (note) {
-      // Single note to play
-      addFallingBlock(note)
-    }
-  }
-
-  function addFallingBlock(note) {
-    const x = notes.indexOf(note) * 85 + 35 + 25
-    const y = 0
-    const block = scene.add.rectangle(x, y, 30, 30, 0xaa4422)
-    scene.physics.add.existing(block)
-    block.body.velocity.y = 600
-  }
-
-  // ms per eigth = (60 s/min) * (1000 ms/s) / BPM / (2 eigths/beat)
-  const eigthNoteMs = (60 * 1000) / SUMMERTIME_BPM / 2
-  const startBeatmap = () =>
-    scene.time.addEvent({
-      delay: eigthNoteMs,
-      callback: nextNote,
-      callbackScope: scene,
-      loop: true,
-    })
-
-  Object.keys(pianoToKeyboard).map(addTargetBlock)
+  Object.keys(PIANO_TO_KEYBOARD).map(addTargetBlock)
   function addTargetBlock(note) {
-    const x = notes.indexOf(note) * 85 + 35 + 25
+    const x = NOTES.indexOf(note) * 85 + 35 + 25
     const y = 525 + 25
     scene.add.rectangle(x, y, 55, 55, 0x66aaee, 0.5)
-    scene.add.text(x - 10, y - 15, pianoToKeyboard[note], { font: '32px' })
+    scene.add.text(x - 10, y - 15, PIANO_TO_KEYBOARD[note], { font: '32px' })
   }
 }
 
