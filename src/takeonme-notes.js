36e6b2b1e68c83c7b5bba900a96261821d698fc5
Eric Shen <email.ericshen@gmail.com>
2021-02-15 19:49:19 -0500

    Cleanup some comments in takeonme-notes.js

--- src/takeonme-notes.js
+++ src/takeonme-notes.js
@@ -1,17 +1,4 @@
 // Courtesy of https://pianoletternotes.blogspot.com/2018/02/take-on-me-by-ha.html
-// ~120 BPM, so each 8th note = ~100 * 1/400ths of a second
-// Maps each tick (1/400sec) to a frequency (hertz)
-// export default {
-//   90: '',
-//   180: '400', // G4 392
-//   280: '400', // G4 392
-//   380: '340', // E4 330
-//   480: '450', // A4 440
-//   580: '400', // G4 392
-//   680: '340', // E4 330
-//   780: '270', // C4 262
-//   880: '300', // D4 294
-// }
 
 // Mapped into G major, but isn't using any flats.
 // TODO: Some notes are missing or off:
