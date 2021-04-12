373bfbe9d1b64fffdb5078f4fc76b71fa5bc42c2
Austin Chen <akrolsmir@gmail.com>
2021-04-12 00:12:50 -0700

    Collect emails via Google forms

--- src/components/SongsList.vue
+++ src/components/SongsList.vue
@@ -15,4 +15,16 @@ export default {
     <router-link :to="`/songs/${song.id}`"> {{ song.name }}</router-link>
     by {{ song.artist }}
   </div>
+  <br />
+  <br />
+  <br />
+  <iframe
+    src="https://docs.google.com/forms/d/e/1FAIpQLSdQQPYvvQwrc7og2qAJsWzAvZTjE3arYmWraPVS4PrLOLbetA/viewform?embedded=true"
+    width="640"
+    height="800"
+    frameborder="0"
+    marginheight="0"
+    marginwidth="0"
+    >Loading…</iframe
+  >
 </template>
